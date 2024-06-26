943f0028e4750e79a4c9cc79aba450aaf2642dfc
shreyashi singh <102822983+sheryshi@users.noreply.github.com>
2024-06-26 21:00:16 +0000

    datatypes

---
+++ 01_javascript/datatypes.js
@@ -0,0 +1,16 @@
+// number=> 2 to power 53
+//bigint
+//string ""
+// boolean=>true/false
+//null =>standalone value
+//undefined=>
+// symbol=>unique
+
+//object
+
+console.log(typeof undefined)// undefined
+console.log(typeof null);// object
+
+// const are the non changable variables
+//let it knows it's scope
+//var it don't know it's scope
