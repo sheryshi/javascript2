84c15ab0a21030798b20867f1247c0e538e5e4f6
shreyashi singh <102822983+sheryshi@users.noreply.github.com>
2024-06-26 21:42:39 +0000

    02_comparison

---
+++ 01_javascript/02_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1)
+
+//basic conversion
+
+console.log("2">1);
+console.log("02">1);
+//must avoid this kind of conversion
+console.log(null>0);
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined==0)
+
+// ===
+
+console.log("2"===2);
+
