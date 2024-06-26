8c63839e8917cb0ab115cf89e98809efae3e8df8
shreyashi singh <102822983+sheryshi@users.noreply.github.com>
2024-06-26 23:05:00 +0000

    string

---
+++ 01_javascript/05_string.js
@@ -0,0 +1,45 @@
+const name="shreyashi"
+const repoCount=50
+
+//console.log(name+repoCount+"value");
+
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName=new String('shreya-shi')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+const newString=gameName.substring(0,4)
+console.log(newString)
+
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString);
+
+
+const newString1="  shreyashi   "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url="https://shreya.com/shreya%90singh"
+console.log(url.replace('%90','-'))
+console.log(url.includes('shreyashi'))
+
+// substring
+//slice
+//trim
+//replace
+//includes
+//split
+
+
+const names=new String('Shreya-shi-singh')
+console.log(names.split('-'))
+
+
+
