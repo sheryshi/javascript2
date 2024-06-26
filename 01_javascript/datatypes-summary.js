453b102b45fcf886ec346c6279b3a8ca6d6e9876
shreyashi singh <102822983+sheryshi@users.noreply.github.com>
2024-06-26 22:22:41 +0000

    datatype-summary

--- 01_javascript/datatypes-summary.js
+++ 01_javascript/datatypes-summary.js
@@ -2,7 +2,7 @@
 
 // 7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
 
-const score=100
+/*const score=100
 const ScoreValue=100.3
 
 const isLoggedIn=false
@@ -36,4 +36,35 @@ const myFunction = function(){
 console.log(typeof bigNumber);
 console.log(typeof outSideTemp)
 console.log(typeof myFunction)
-console.log(typeof heros)
+console.log(typeof heros)*/
+
+
+
+
+// ++++++++++++++++++++++++++++++++
+
+//stack(primitive), Heap(Non-primitive)
+
+let myname="shreyashisingh"
+
+let anothername=myname
+anothername="code"
+
+console.log(anothername);
+console.log(myname)
+
+let user1={
+    email:"user@google.com",
+    upi:"user@yb"
+}
+
+let user2=user1
+
+user2.email="shreyashi@gmail.com"
+
+console.log(user1.email)
+console.log(user2.email)
+
+// jitne bhi primitive data type ate hai wo stack me jate hai 
+// jitne bhi non-primitive datatype ate hai wo heap me jate hai yha pe 
+// jo khuch bhi value hum rakhte hai uska refrence milta hai
